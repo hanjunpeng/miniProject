16829079a22e77c81c63d293dd3c9f29b33b5ba1
hjp <hanjunpeng@yeah.net>
2019-06-15 10:38:35 +0800

    commit

---
+++ server/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  serverPort: '',
+
+  // 小程序 appId 和 appSecret
+  appId: '',
+  appSecret: '',
+  
+  // mongodb连接配置， 生产环境请使用更复杂的用户名密码
+  mongoHost: '',
+  mongoPort: '',
+  mongoUser: '',
+  mongoPass: '',
+  mongoDb: ''
+}
